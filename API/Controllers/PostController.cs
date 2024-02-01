3e66c35f44dbbf126f5451fb13f7097f77e83690
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-02-01 15:54:55 +0100

    demo post api

---
+++ API/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models.Entities;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet("demos")]
+        public List<Post> Demos()
+        {
+            return new List<Post>();
+        }
+    }
+}
