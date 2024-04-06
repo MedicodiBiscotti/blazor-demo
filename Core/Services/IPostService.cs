36b5ce898902e7d44ff5f895807e33241bd540f4
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-04-06 18:49:06 +0200

    comment on usage of generic services

--- Core/Services/IPostService.cs
+++ Core/Services/IPostService.cs
@@ -3,6 +3,9 @@ using Model.Entities;
 
 namespace Core.Services;
 
+// Can either extend the generic service interface, or add its own methods.
+// Implementation can then also choose to extend the generic, or compose the generic method service.
+// Or both.
 public interface IPostService : IGenericClassCrudService<Post, int, PostDto, PostDto, PostDto>
 {
 }
