606f1d5778ade7deebf02e09dd2dd47ceff96906
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-04-12 23:44:44 +0200

    adds fluentui and mudblazor

--- BlazorDemo/BlazorDemo/Program.cs
+++ BlazorDemo/BlazorDemo/Program.cs
@@ -1,35 +1,53 @@
 using BlazorDemo.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 using _Imports = BlazorDemo.Client._Imports;
+using MudBlazor.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace BlazorDemo;
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseWebAssemblyDebugging();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        builder.Services.AddScoped(_ => new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5055")
+        });
+
+        builder.Services.AddMudServices();
+        builder.Services.AddFluentUIComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error", true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(_Imports).Assembly);
+
+        app.Run();
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(_Imports).Assembly);
-
-app.Run();
